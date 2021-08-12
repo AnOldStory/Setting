074c69dc2429219a2bc74d588815bcc2c218d0c7
HyeongChang Lee <hc9904@hanyang.ac.kr>
2021-08-12 23:13:07 +0900

    fix theme download directory

--- Setting-Manager/Setting-Manager_anoldstory.sh
+++ Setting-Manager/Setting-Manager_anoldstory.sh
@@ -39,6 +39,6 @@ echo 3. import setting
 import_setting(){
     cd ~/
     mkdir -p ~/.vim/colors
-    wget "https://www.vim.org/scripts/download_script.php?src_id=26557" -O jellybeans.vim
+    wget "https://www.vim.org/scripts/download_script.php?src_id=26557" -P ~/.vim/colors -O jellybeans.vim
     wget $setting_list -qO - | jq '.[0].download_url' | xargs wget
 }

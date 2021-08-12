19cf205be168d3fa4c126402cebfd1fc4be3e370
HyeongChang Lee <hc9904@hanyang.ac.kr>
2021-08-13 08:10:17 +0900

    Update Setting-Manager_anoldstory.ps1

--- Setting-Manager/Setting-Manager_anoldstory.ps1
+++ Setting-Manager/Setting-Manager_anoldstory.ps1
@@ -3,7 +3,7 @@
 
 <###### Elevate for admin  ######>
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
-  { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+  { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"https://raw.githubusercontent.com/AnOldStory/Setting/master/Setting-Manager/Setting-Manager_anoldstory.ps1`"" -Verb RunAs; exit }
 
 <###### Configure ######>
 $username = "thesky"
